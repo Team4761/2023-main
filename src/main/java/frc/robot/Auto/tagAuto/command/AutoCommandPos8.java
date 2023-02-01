79008b2d2819a76bd36a991e44ba596414713e56
Bpro1 <114193897+Bpro1@users.noreply.github.com>
2023-01-31 20:46:23 -0500

    Bare bones auto
    some shell stuff

---
+++ src/main/java/frc/robot/Auto/tagAuto/command/AutoCommandPos8.java
@@ -0,0 +1,11 @@
+package frc.robot.Auto.tagAuto.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutoCommandPos8 extends SequentialCommandGroup {
+
+    public AutoCommandPos8(){
+        super();
+    }
+}
