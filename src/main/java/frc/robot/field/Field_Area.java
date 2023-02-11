3f962c919a7e66b7bf3d25fba7a06ebbdf185ec5
Erik Currin <ehc12345678@gmail.com>
2023-02-11 15:36:17 -0500

    move arm

--- src/main/java/frc/robot/field/Field_Area.java
+++ src/main/java/frc/robot/field/Field_Area.java
@@ -3,20 +3,55 @@ package frc.robot.field;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class Field_Area {
-    private final Translation2d BottemLeft;
-    private final Translation2d TopRight;
+    private final Translation2d bottomLeft;
+    private final Translation2d topRight;
 
     public Field_Area(double x1, double y1, double x2, double y2){
-        BottemLeft = new Translation2d (x1,y1);
-        TopRight = new Translation2d (x2,y2);
+        bottomLeft = new Translation2d (x1,y1);
+        topRight = new Translation2d (x2,y2);
     }
 
-
-    public Translation2d getBottemLeft() {
-        return BottemLeft;
+    public Translation2d getBottomLeft() {
+        return bottomLeft;
     }
 
     public Translation2d getTopRight() {
-        return TopRight;
+        return topRight;
+    }
+
+    public Translation2d getBottomRight() {
+        return new Translation2d(topRight.getX(), bottomLeft.getY());
+    }
+
+    public Translation2d getTopLeft() {
+        return new Translation2d(bottomLeft.getX(), topRight.getY());
+    }
+
+    public Translation2d getCenter() {
+        return new Translation2d(bottomLeft.getX() + getWidth() / 2, bottomLeft.getY() + getHeight() / 2);
+    }
+
+    public Translation2d getCenterLeft() {
+        return new Translation2d(bottomLeft.getX(), getCenter().getY());
+    }
+
+    public Translation2d getCenterRight() {
+        return new Translation2d(topRight.getX(), getCenter().getY());
+    }
+
+    public Translation2d getTopCenter() {
+        return new Translation2d(getCenter().getX(), topRight.getY());
+    }
+
+    public Translation2d getBottomCenter() {
+        return new Translation2d(getCenter().getX(), bottomLeft.getY());
+    }
+
+    public double getWidth() {
+        return bottomLeft.getX() - topRight.getX();
+    }
+
+    public double getHeight() {
+        return bottomLeft.getY() - topRight.getY();
     }
 }
