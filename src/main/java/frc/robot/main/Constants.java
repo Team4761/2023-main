88c029e59bb6b851df37dcf4c2f753c8d893a6f8
Kugelblitz31 <55564274+Kugelblitz31@users.noreply.github.com>
2023-02-12 13:22:40 -0500

    added real time edits to PID and april tag pose goals, and way to activate command

--- src/main/java/frc/robot/main/Constants.java
+++ src/main/java/frc/robot/main/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot.main;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Constants {
 
     public static final double drivetrainGearRatio = 0.01; //TODO to be determined, will be degrees of CANCoder to distance
@@ -9,13 +12,15 @@ public class Constants {
 
 
     //PID constants for robot.
-    public static final double LINEAR_P = 0.01;
-    public static final double LINEAR_I = 0;
-    public static final double LINEAR_D = 0.1;
+    public static  double LINEAR_P = 0.01;
+    public static  double LINEAR_I = 0;
+    public static  double LINEAR_D = 0.1;
 
-    public static final double ANGULAR_P = 0.01;
-    public static final double ANGULAR_I = 0;
-    public static final double ANGULAR_D = 0.1;
+    public static  double ANGULAR_P = 0.01;
+    public static  double ANGULAR_I = 0;
+    public static  double ANGULAR_D = 0.1;
+
+    public static Pose2d goalPose = new Pose2d(0,0,new Rotation2d(0,0));
 
 
 
