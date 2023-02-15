581c5f173621f393d6735a7086dbdea958ea1687
ehc12345678 <ehc12345678@gmail.com>
2023-02-14 20:13:51 -0500

    Snapshot with linear measurements

--- src/main/java/frc/robot/command/MoveStraightMeasuredCommand.java
+++ src/main/java/frc/robot/command/MoveStraightMeasuredCommand.java
@@ -9,13 +9,16 @@ import frc.robot.impl.placeholder.Placeholder;
 import frc.robot.main.Robot;
 
 public class MoveStraightMeasuredCommand extends CommandBase {
-    final int kCountsPerRev = 4096;  //Encoder counts per revolution of the motor shaft.
-    final double kGearRatio = 1.0;
+    final int kCountsPerRev = 2048;  //Encoder counts per revolution of the motor shaft.
+    final double kGearRatio = 3.1;
     final double kWheelRadiusInches = 2;
+    final double linearSpeedIncrease = .02;
     final double kMaxOutput = RobotDriveBase.kDefaultMaxOutput;
 
     private final Distances start;
-    private final double speed;
+    private final double goalSpeed;
+
+    private double speed = 0.0;
     private final double meters;
 
     /**
@@ -24,37 +27,62 @@ public class MoveStraightMeasuredCommand extends CommandBase {
      */
     public MoveStraightMeasuredCommand(double xSpeed, double meters) {
         start = Robot.impl.getSensorReadings();
-        speed = MathUtil.applyDeadband(xSpeed, RobotDriveBase.kDefaultDeadband);
+        goalSpeed = MathUtil.applyDeadband(xSpeed, RobotDriveBase.kDefaultDeadband);
         this.meters = meters;
     }
 
     @Override
     public void execute() {
+        // TODO: linear interpolation for speed
+        if (speed < goalSpeed) {
+            speed += linearSpeedIncrease;
+        }
         var speeds = DifferentialDrive.tankDriveIK(speed, speed, true);
 
         var delta = getDelta();
         var avgDeltaLeftRight = (delta.frontLeft - delta.frontRight) / 2;
 
-        // the closer the average delta is to zero, the more we want speeds.left to stay the same.
-        // if the average delta is positive, we want left to slow down just a bit, and go to zero if it's a full
-        // revolution behind.
-        speeds.left *= 1 - (avgDeltaLeftRight / 4096);
+        if ((count++ % 10) == 0) {
+            System.out.println("delta=" + delta);
+            System.out.println("avgDelta=" + avgDeltaLeftRight + " start speeds=" + speeds.left + " speed right=" + speeds.right);
+        }
 
-        // the closer the average delta is to zero, the more we want speeds.left to stay the same
-        // if the average delta is positive, we want right to speed up just a bit, and go to 2*speed if it's a full
-        // revolution behind.
-        speeds.right *= 1 + (avgDeltaLeftRight / 4096);
+        final double nudge = .02;
+        if (avgDeltaLeftRight > 0) {
+            speeds.left *= (1.0 - nudge);
+            //speeds.right *= (1.0 + nudge);
+        } else {
+            speeds.left *= (1.0 + nudge);
+            //speeds.right *= (1.0 - nudge);
+        }
+
+//        // the closer the average delta is to zero, the more we want speeds.left to stay the same.
+//        // if the average delta is positive, we want left to slow down just a bit, and go to zero if it's a full
+//        // revolution behind.
+//        speeds.left *= 1 - (avgDeltaLeftRight / kCountsPerRev);
+//
+//        // the closer the average delta is to zero, the more we want speeds.left to stay the same
+//        // if the average delta is positive, we want right to speed up just a bit, and go to 2*speed if it's a full
+//        // revolution behind.
+//        speeds.right *= 1 + (avgDeltaLeftRight / kCountsPerRev);
+
+        System.out.println("Speed left=" + speeds.left + " speed right=" + speeds.right);
 
         // TODO: this can be extracted to RobotImpl
         Placeholder.m_leftMotors.set(speeds.left * kMaxOutput);
         Placeholder.m_rightMotors.set(speeds.right * kMaxOutput);
     }
 
+    int count = 0;
     @Override
     public boolean isFinished() {
         Distances delta = getDelta();
         var distance = ticksToMeters(delta.average());
-        return distance > this.meters;
+        boolean finished = distance > this.meters;
+        if (finished) {
+            System.out.println(count + " Finished after " + distance + " meters " + Robot.impl.getSensorReadings());
+        }
+        return finished;
     }
 
     private Distances getDelta() {
