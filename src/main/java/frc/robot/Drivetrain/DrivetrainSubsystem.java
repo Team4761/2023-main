c726f0a89df988f706da21ad422799c948b879e4
ehc12345678 <ehc12345678@gmail.com>
2023-02-18 16:31:30 -0500

    checkpoint

--- src/main/java/frc/robot/Drivetrain/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/Drivetrain/DrivetrainSubsystem.java
@@ -1,10 +1,20 @@
 package frc.robot.Drivetrain;
 
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.impl.placeholder.Placeholder;
 import frc.robot.main.Robot;
 
 public class DrivetrainSubsystem extends SubsystemBase {
+    private final double WHEEL_DIAMETER_METERS = Units.inchesToMeters(2);
+    private final double TICKS_PER_ROTATION = 2048;
+    private final double GEAR_RATIO = 10.90909090; // 120 / 11
+    private final double TICKS_PER_METER = (double)(TICKS_PER_ROTATION * GEAR_RATIO) * (double)(1 / (WHEEL_DIAMETER_METERS * Math.PI)); //46644.183
+    private final double METERS_PER_TICK = 1 / TICKS_PER_METER; //2.149e-5
 
     // With eager singleton initialization, any static variables/fields used in the
     // constructor must appear before the "INSTANCE" variable so that they are initialized
@@ -16,6 +26,10 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * than trying to construct an instance of this class.)
      */
     private final static DrivetrainSubsystem INSTANCE = new DrivetrainSubsystem();
+
+    public final static Placeholder test = new Placeholder();
+    private final DifferentialDriveOdometry m_odometry;
+
 
     /**
      * Returns the Singleton instance of this DrivetrainSubsystem. This static method
@@ -33,16 +47,39 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * the {@link #getInstance()} method to get the singleton instance.
      */
     private DrivetrainSubsystem() {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+        m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0), 0, 0);
+
     }
 
-    public void arcadeDrive(double speed, double rotation){
+    @Override
+    public void periodic() {
+        super.periodic();
+        m_odometry.update(
+                Rotation2d.fromDegrees(-getAngle()),
+                -Placeholder.front_left.getSelectedSensorPosition() * METERS_PER_TICK,
+                Placeholder.front_right.getSelectedSensorPosition() * METERS_PER_TICK);
+    }
+
+    public void resetOdometry() {
+        resetAngle();
+        Placeholder.front_left.setSelectedSensorPosition(0);
+        Placeholder.front_right.setSelectedSensorPosition(0);
+        m_odometry.resetPosition(Rotation2d.fromDegrees(0), 0, 0, new Pose2d());
+    }
+
+    private void resetAngle() {
+        Placeholder.m_gyro.reset();
+    }
+
+    private Double getAngle() {
+        return Placeholder.m_gyro.getAngle();
+    }
+
+    public void arcadeDrive(double speed, double rotation) {
         Robot.impl.getDrive().arcadeDrive(speed, rotation);
     }
-    public void tankDrive(double rightSpeed, double leftSpeed){
+
+    public void tankDrive(double rightSpeed, double leftSpeed) {
         Robot.impl.getDrive().tankDrive(rightSpeed, leftSpeed);
     }
 }
--- src/main/java/frc/robot/impl/placeholder/Placeholder.java
+++ src/main/java/frc/robot/impl/placeholder/Placeholder.java
@@ -1,14 +1,20 @@
 package frc.robot.impl.placeholder;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import frc.robot.command.Distances;
 import frc.robot.impl.RobotImpl;
 import frc.robot.main.Constants;
-import frc.robot.main.Varyings;
 
 public class Placeholder extends RobotImpl {
     // Drivetrain motors. The Talons already have encoders inside them
@@ -17,8 +23,15 @@ public class Placeholder extends RobotImpl {
     public static WPI_TalonFX back_left = new WPI_TalonFX(7);
     public static WPI_TalonFX back_right = new WPI_TalonFX(1);
 
+    //Gyro and PID controllers for PID
+    public static ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+    private final PIDController left_PIDController = new PIDController(.1, 0, .001);
+
+    private final PIDController right_PIDController = new PIDController(-2.5, 0, .001);
+
     //Encoders that are pre-built into the talons
-    //public static CANCoder front_right_CANCoder = new CANCoder(0);
+//    public static CANCoder front_left_CANCoder = new CANCoder(0);
 //    public static CANCoder front_right_CANCoder = new CANCoder(1);
 //    public static CANCoder back_left_CANCoder = new CANCoder(2);
 //    public static CANCoder back_right_CANCoder = new CANCoder(3);
@@ -26,22 +39,27 @@ public class Placeholder extends RobotImpl {
 //    //Setting encoder variables
 //    CANCoderConfiguration CANCoderConfig = new CANCoderConfiguration();
 //
-    //private static final Encoder m_leftEncoder = new Encoder(0, 1);
-
 
     // Drivetrain
-    public static final MotorControllerGroup m_leftMotors = new MotorControllerGroup(front_left,back_left);
-    public static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(front_right,back_right);
-//    public static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(front_right);
-    private static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors,m_rightMotors);
+    public static final MotorControllerGroup m_leftMotors = new MotorControllerGroup(front_left, back_left);
+    public static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(front_right, back_right);
+    //    public static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(front_right);
+    private static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
     //PID controllers (for auto stuff, maybe drive)
     public static final PIDController linear_PIDcontroller = new PIDController(Constants.LINEAR_P, Constants.LINEAR_I, Constants.LINEAR_D);
     public static final PIDController angular_PIDcontroller = new PIDController(Constants.ANGULAR_P, Constants.ANGULAR_I, Constants.ANGULAR_D);
 
-    public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
-    public Placeholder(){
+    private final DifferentialDriveKinematics m_kinematics =
+            new DifferentialDriveKinematics(Constants.trackWidth);
+
+    //To be determined
+    private final SimpleMotorFeedforward m_feedFoward = new SimpleMotorFeedforward(0.106, 0.76);
+
+    public static boolean doLogging = false;
+
+    public Placeholder() {
 
         //config of the encoders
 
@@ -58,18 +76,24 @@ public class Placeholder extends RobotImpl {
 
         getDrive().setExpiration(.3);
 //        m_rightMotors.setInverted(true);
-        m_leftMotors.setInverted(true);
-        setPidContants(front_left);
-        setPidContants(front_right);
-        setPidContants(back_left);
-        setPidContants(back_right);
-        gyro.calibrate();
-    }
 
-    private void setPidContants(WPI_TalonFX motor) {
-        motor.config_kP(0, Varyings.drivetrainpid.getP());
-        motor.config_kI(0, Varyings.drivetrainpid.getI());
-        motor.config_kD(0, Varyings.drivetrainpid.getD());
+        m_leftMotors.setInverted(true);
+
+        front_left.configFactoryDefault();
+        back_left.configFactoryDefault();
+        front_right.configFactoryDefault();
+        back_right.configFactoryDefault();
+        front_left.setNeutralMode(NeutralMode.Brake);
+        back_left.setNeutralMode(NeutralMode.Brake);
+        front_right.setNeutralMode(NeutralMode.Brake);
+        back_right.setNeutralMode(NeutralMode.Brake);
+
+        front_left.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+        front_right.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+        back_left.set(ControlMode.Follower, front_left.getDeviceID());
+        back_right.set(ControlMode.Follower, front_left.getDeviceID());
+
+        linear_PIDcontroller.setSetpoint(0);
     }
 
     @Override
@@ -89,17 +113,66 @@ public class Placeholder extends RobotImpl {
         return degrees / 90 * speed * ratio;
     }
 
-    @Override
-    public Distances getSensorReadings() {
-        return new Distances(
-            getSensorPosition(front_left),
-            getSensorPosition(front_right),
-            getSensorPosition(back_left),
-            getSensorPosition(back_right)
-        );
+    public static void logMySpeed() {
+        log(front_left, "front_left");
+        log(front_right, "front_right");
+        log(back_left, "back_left");
+        log(back_right, "back_right");
+        System.out.println();
     }
 
-    private double getSensorPosition(WPI_TalonFX motor) {
-        return motor.getSensorCollection().getIntegratedSensorPosition();
+    public static void log(WPI_TalonFX motor, String label) {
+        if (doLogging) {
+            System.out.println(
+                    label + ": v=" + motor.getSensorCollection().getIntegratedSensorVelocity() +
+                            "  pos=" + motor.getSensorCollection().getIntegratedSensorPosition()
+            );
+        }
+
+    }
+
+    public static void logGyro() {
+        System.out.println(m_gyro.getAngle());
+    }
+
+    public static void zeroEncoders() {
+        front_right.getSensorCollection().setIntegratedSensorPosition(0, 100);
+        front_left.getSensorCollection().setIntegratedSensorPosition(0, 100);
+        back_right.getSensorCollection().setIntegratedSensorPosition(0, 100);
+        back_left.getSensorCollection().setIntegratedSensorPosition(0, 100);
+    }
+
+
+    public static double averageMotorGroupVelocity(WPI_TalonFX front_motor, WPI_TalonFX back_motor) {
+        //Needs to account for gear ratio
+        return ((front_motor.getSensorCollection().getIntegratedSensorVelocity() + back_motor.getSensorCollection().getIntegratedSensorVelocity()) / 2);
+    }
+
+    public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+        final double leftFeedforward = m_feedFoward.calculate(speeds.leftMetersPerSecond);
+        final double rightFeedforward = m_feedFoward.calculate(speeds.rightMetersPerSecond);
+
+        final double leftOutput =
+                left_PIDController.calculate(nativeUnitsToDistanceMeters(Placeholder.averageMotorGroupVelocity(front_left, back_left)), 1);
+        final double rightOutput =
+                right_PIDController.calculate(nativeUnitsToDistanceMeters(Placeholder.averageMotorGroupVelocity(front_right, back_right)), 1);
+
+        System.out.println("Right Voltage: " + (rightOutput + rightFeedforward));
+        System.out.println("Left Voltage: " + (leftFeedforward + leftOutput));
+
+        System.out.println("Right " + averageMotorGroupVelocity(front_right, back_right));
+        System.out.println("Left " + averageMotorGroupVelocity(front_left, back_left));
+    }
+
+    public void drive(double xSpeed, double rot) {
+        var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+        setSpeeds(wheelSpeeds);
+    }
+
+    private double nativeUnitsToDistanceMeters(double sensorCounts) {
+        double motorRotations = (double) sensorCounts / Constants.talonEncoderResolution;
+        double wheelRotations = motorRotations / Constants.drivetrainGearRatio;
+        double positionMeters = wheelRotations * (2 * Math.PI * Units.inchesToMeters(Constants.wheelRadius));
+        return positionMeters;
     }
 }
