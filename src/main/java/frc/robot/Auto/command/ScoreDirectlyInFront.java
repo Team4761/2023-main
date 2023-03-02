45af1a14da98717e0aea4a1056bbd9e41f32f160
Mguarr <110061423+Mguarr@users.noreply.github.com>
2023-03-01 19:16:09 -0500

    Useless

--- src/main/java/frc/robot/Auto/command/ScoreDirectlyInFront.java
+++ src/main/java/frc/robot/Auto/command/ScoreDirectlyInFront.java
@@ -3,4 +3,6 @@ package frc.robot.Auto.command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class ScoreDirectlyInFront extends SequentialCommandGroup {
+    public ScoreDirectlyInFront(){
+    }
 }
