e473f0370f06424244561b8b18b94becce96801e
EggyMath <55564321+EggyMath@users.noreply.github.com>
2023-03-01 19:32:34 -0500

    Update Constants.java

--- src/main/java/frc/robot/main/Constants.java
+++ src/main/java/frc/robot/main/Constants.java
@@ -31,8 +31,8 @@ public class Constants {
     public static final double trackWidth = 19.5;//Inches : 19.5 //Meters : 0.4953
 
     // Arm Constants
-    static final public double ARM_LENGTH_BOTTOM = 22.5;         // Bottom Arm?  //now 22.5 was 25
-    static final public double ARM_LENGTH_TOP = 28.5;            // Top Arm?     //now 28.5 was 30
+    static final public double ARM_LENGTH_BOTTOM = 22.5;         // Bottom Arm?
+    static final public double ARM_LENGTH_TOP = 39.0;            // Top Arm?     //now 39.0 was 28.5
     static public double ARM_P_TOP = 0.6;                        // PID control for arms
     static public double ARM_I_TOP = 0.0;                        // PID control for arms
     static public double ARM_D_TOP = 0.1;                        // PID control for arms
